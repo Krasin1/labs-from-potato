33b33d3fb1e11ac627795e85f4a11139d2bc88fa
Krasin1 <dunaevv84@gmail.com>
2023-10-24 21:58:39 +0300

    prolog lab1 otchet

---
+++ 5/java/lr02/main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.text.*;
+
+class Pc {
+    public int id;
+    public String vendor;
+    public String cpu;
+    public int freq;
+    public short ram;
+    public int storage;
+    public Date release;
+    public int cost;
+    public int count;
+}
+
+class MVector {
+    
+}
+
+class Main {
+    public static void main(String[] args){
+        // SimpleDateFormat a = new SimpleDateFormat("yyyy-MM-dd");
+        // String date = a.format(new Date());
+        // System.out.println(date);
+        // try {
+        //     Date nah = a.parse("2022-01-12");
+        //     // System.out.println("date is : " + a.format().parse("2023-10-09"));
+        // } catch (ParseException e) { e.printStackTrace(); }
+    }
+}
