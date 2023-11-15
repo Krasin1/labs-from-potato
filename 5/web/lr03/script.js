7d72863b15cefa08826c4f3ab7b49dbbdada0da4
Krasin1 <dunaevv84@gmail.com>
2023-11-15 07:52:25 +0300

    web lab3 code finish

--- 5/web/lr03/script.js
+++ 5/web/lr03/script.js
@@ -1,13 +1,3 @@
-var emailBox = document.getElementByName("mailCheck");
-//
-emailBox.addEventListener("submit", function(event){
-    alert("lol");
-    if(event.key == "Enter") {
-        alert("lol");
-        document.getElementById("b").click();
-    }
-})
-
 function email(elem) {
     let mail = new String(elem.value);
     if (mail.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -16,4 +6,15 @@ function email(elem) {
     }
     document.getElementById("result").innerHTML = "Адрес неверный";
 }
+let flag = 0;
+function show_information() { alert("Дунаев В.Е. Группа ИКПИ-11 Практическая №4"); }
 
+function color() {
+    if(flag == 0) {
+        document.body.style.backgroundColor = "#fff700";
+        flag = 1;
+    } else{
+        document.body.style.backgroundColor = 'white';
+        flag = 0;
+    }
+}
